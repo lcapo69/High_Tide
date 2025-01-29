da29bfc276413ea575c94c153de21ec5cc142fe6
lcapo69 <111012764+lcapo69@users.noreply.github.com>
2025-01-29 16:18:35 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+function checkAge() {
+  let age = prompt("Please enter your age:");
+
+  if (age >= 21) {
+    alert("Welcome!");
+  } else {
+    alert("You must be 21 or older to access this content.");
+  }
+}
+
+checkAge(); 
+
 const myCarouselElement = document.querySelector('#myCarousel')
 
 const carousel = new bootstrap.Carousel(myCarouselElement, {
